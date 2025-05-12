c6ca46104eac91cd89d37c2175f4dfa6ca32ec00
svizcaino26 <svizcaino26@gmail.com>
2025-05-12 14:27:14 -0500

    working of handlers testing

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -18,6 +19,7 @@ var tests = []struct {
 	params             []postData
 	expectedStatusCode int
 }{
+	// [GET TESTS]
 	{"home", "/", "GET", []postData{}, http.StatusOK},
 	{"about", "/about", "GET", []postData{}, http.StatusOK},
 	{"gq", "/generals-quarters", "GET", []postData{}, http.StatusOK},
@@ -25,7 +27,22 @@ var tests = []struct {
 	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
-	// {"rs", "/reservation-summary", "GET", []postData{}, http.StatusOK},
+
+	// [POST TESTS]
+	{"post-sa", "/search-availability", "POST", []postData{
+		{key: "start", value: "2025-07-01"},
+		{key: "end", value: "2025-07-10"},
+	}, http.StatusOK},
+	{"post-sa-j", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2025-07-01"},
+		{key: "end", value: "2025-07-10"},
+	}, http.StatusOK},
+	{"post-mr", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "John"},
+		{key: "last_name", value: "Smith"},
+		{key: "email", value: "test@mail.com"},
+		{key: "phone", value: "555-555-5555"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -41,6 +58,19 @@ func TestHandlers(t *testing.T) {
 			}
 			if resp.StatusCode != e.expectedStatusCode {
 				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+				resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+				if err != nil {
+					t.Log(err)
+					t.Fatal(err)
+				}
+				if resp.StatusCode != e.expectedStatusCode {
+					t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+				}
 			}
 		}
 	}
